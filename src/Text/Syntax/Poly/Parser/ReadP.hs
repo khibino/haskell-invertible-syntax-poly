fe6f1a085e4045342d05b8fcb2975356034e1c0d
HIBINO Kei <hibi@utmc.or.jp>
2012-06-11 10:35:28 +0900

    Disable a needless extension.

--- src/Text/Syntax/Poly/Parser/ReadP.hs
+++ src/Text/Syntax/Poly/Parser/ReadP.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances, UndecidableInstances #-}
 
 ----------------------------------------------------------------------------
