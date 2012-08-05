bfee4383aa2cf0146c37cd927f5192685ae06ecd
Kei Hibino <hibi@utmc.or.jp>
2012-08-05 15:28:30 +0900

    Update haddocks.

--- src/Text/Syntax/Poly/Type.hs
+++ src/Text/Syntax/Poly/Type.hs
@@ -9,14 +9,14 @@
 -- Stability   : experimental
 -- Portability : unknown
 --
--- Type synonyms to represent syntax which has forall type value.
+-- Type synonyms to represent 'Syntax' types which has forall type value.
 
 module Text.Syntax.Poly.Type (
-  -- * Syntax type
+  -- * 'Syntax' type
   SyntaxT,
-  -- * Type for runParser \/ runPrinter from syntax
-  RunAsParser, RunAsPrinter,
-  RunAsParserM, RunAsPrinterM,
+  -- * Type to run syntax as Parser \/ Printer.
+  RunAsParser, RunAsParserM,
+  RunAsPrinter, RunAsPrinterM,
   -- * Error string type
   ErrorString, errorString
   ) where
@@ -26,14 +26,20 @@ import Text.Syntax.Poly.Class (Syntax)
 -- | Syntax type synonym includes contexts. Rank2Types extension is needed.
 type SyntaxT tok a = forall delta . Syntax tok delta => delta a
 
+-- | Type to run syntax as parser
 type RunAsParser     tok tks a e = SyntaxT tok a -> tks -> Either e a
+-- | Same as 'RunAsParser' other than with computation @m@
 type RunAsParserM  m tok tks a e = SyntaxT tok a -> tks -> m (Either e a)
+
+-- | Type to run syntax as printer
 type RunAsPrinter    tok tks a e = SyntaxT tok a -> a   -> Either e tks
+-- | Same as 'RunAsPrinter' other than with computation @m@
 type RunAsPrinterM m tok tks a e = SyntaxT tok a -> a   -> m (Either e tks)
 
 -- | String type which is Show instance not to show but just return string
 newtype ErrorString = ErrorString String
 
+-- | Construct ErrorString
 errorString :: String -> ErrorString
 errorString =  ErrorString
 
